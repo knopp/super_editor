b261a04d9739ad0a6a2d518e91af8a1f8c58208e
Matej Knopp <matej.knopp@gmail.com>
2024-06-17 11:17:11 +0200

    Experimental sliver based layout

--- super_editor/lib/src/default_editor/document_gestures_touch_android.dart
+++ super_editor/lib/src/default_editor/document_gestures_touch_android.dart
@@ -1263,7 +1263,7 @@ class _AndroidDocumentTouchInteractorState extends State<AndroidDocumentTouchInt
       children: [
         // Layer below
         RawGestureDetector(
-          behavior: HitTestBehavior.translucent,
+          behavior: HitTestBehavior.opaque,
           gestures: <Type, GestureRecognizerFactory>{
             TapSequenceGestureRecognizer: GestureRecognizerFactoryWithHandlers<TapSequenceGestureRecognizer>(
               () => TapSequenceGestureRecognizer(),
